ba252efe6ba009fe29edc33a5579ed4b0931109d
Luc Jaubert <lucjaubert@gmail.com>
2023-02-01 09:31:42 +0100

    database connected

--- backend/index.js
+++ backend/index.js
@@ -4,6 +4,16 @@ const app = require("./src/app");
 
 const port = parseInt(process.env.APP_PORT ?? "5000", 10);
 
+const db = require("./config");
+
+db.connect((err) => {
+  if (err) {
+    console.log(err);
+  } else {
+    console.log("Connected to database");
+  }
+});
+
 app.listen(port, (err) => {
   if (err) {
     console.error("Something bad happened");
